fe8c313c43d0bbd4af03cbe199bf6500a398c9ca
Jonathan <56617776+qb1968@users.noreply.github.com>
2019-11-15 12:47:07 -0500

    functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,38 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(arg1, arg2, cb) {
+  console.log(cb(arg1,arg2));
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = function(arg1,arg2){
+  return arg1 + arg2;
+}
 
+let multiply = function(arg1,arg2){
+  return arg1 * arg2;
+}
+
+let greeting = function(first,last){
+  return `Hello ${first} ${last}, nice to meet you!` ;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: can use variables that are not passed explicitly as arguments
 
 
 const external = "I'm outside the function";
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -160,8 +160,12 @@ console.log(lowPopulation);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-// const populationTotal = 0;
+const populationTotal = 0;
+let populationTotal = zooAnimals.reduce((populationTotal, populationAmount) => {
+  return populationTotal + populationAmount.population;
+}, 0);
 
+console.log(populationTotal);
 
 /*
 
